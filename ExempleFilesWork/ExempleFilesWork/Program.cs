e102ce049e24fc4618619c11e54dca2235253998
anythingiscode <rolkawa99@gmail.com>
2022-10-20 01:16:13 +0200

    StreamReader-Writer Class File

--- ExempleFilesWork/ExempleFilesWork/Program.cs
+++ ExempleFilesWork/ExempleFilesWork/Program.cs
@@ -24,11 +24,63 @@
         }
         catch (Exception e)
         {
-            Console.WriteLine($"Hubo un error diferente de no encuentro fichero: {e}");
+            Console.WriteLine($"Hubo un error diferente a 'no encuentro fichero' :\n {e}");
         }
 
 
-        //ESCRITURA DE FICHERO
+        /*  ESCRITURA DE FICHERO
+         *  Si el fichero no existe se crea
+         *  Si el fichero existe se sobreescribe, pero! se puede controlar para añadir en lugar de sobreescribir
+         */
+
+        string[] writerText = new string[] { "El texto que quiero que aparezca es:",
+                                        "Barcelona",
+                                        "Girona",
+                                        "Lleida",
+                                        "Tarragona" };
+        try
+        {
+            using (StreamWriter writer = new StreamWriter(@"C:\ROL\C#\Temp\EjemploFicheros\ExempleFichierEscritura_1.txt"))
+            {       
+                foreach (string txt in writerText)
+                {
+                    writer.WriteLine(txt);
+                }
+               
+                writer.Close();             // CERRAR EL FICHERO SIEMPRE!!! Sino no se guarda el fichero. En este caso está dentro del using y no es necesario
+                
+                Console.WriteLine("\n\nFin de escritura de fichero.");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Hubo un error al escribir el fichero :\n {e}");
+        }
+
+        // Si no pongo el código dentro de un using --> 
+        // writer.Close();
+
+        // LECTURA Y ESCRITURA USANDO LA CLASE "FILE"
+        // LECTURA
+
+        string fileRead = File.ReadAllText(@"C:\ROL\C#\Temp\EjemploFicheros\ExempleFichier_1.txt");     //Lee todo el texto
+        Console.WriteLine(fileRead);
+        //Lectura por lineas  --> Ojo ReadAllLines devuelve un array de strings []
+        string[] fileReadByLines = File.ReadAllLines(@"C:\ROL\C#\Temp\EjemploFicheros\ExempleFichier_1.txt");     //Lee todo el texto
+        foreach (string line in fileReadByLines) 
+            Console.WriteLine(line);
+
+        //ESCRITURA
+        string writeTextAllText = "Text a escriure pel segon cas.";
+
+        File.WriteAllLines(@"C:\ROL\C#\Temp\EjemploFicheros\ExempleFichierEscritura_2.txt", writerText);     //Lee todo el texto
+        File.WriteAllText(@"C:\ROL\C#\Temp\EjemploFicheros\ExempleFichierEscritura_3.txt", writeTextAllText);
+
+        //Con la clase "File" se puede añadir texto directamente:
+        //Linea a linea
+        File.AppendAllLines(@"C:\ROL\C#\Temp\EjemploFicheros\ExempleFichierEscritura_2.txt", writerText);
+        //Todo el txt de un golpe (nota: añadí un salto de linea antes de añadir el texto)
+        File.AppendAllText(@"C:\ROL\C#\Temp\EjemploFicheros\ExempleFichierEscritura_2.txt", "\n" + writeTextAllText);
 
         Console.ReadKey();
     }
