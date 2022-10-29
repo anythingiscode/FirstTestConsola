df49887e8b3b1fa1ab929a3d11ef3db9e4bd79e6
anythingiscode <rolkawa99@gmail.com>
2022-10-29 14:09:50 +0200

    Contar palabras

--- EjerciciosBásicos/EjerciciosBásicos/Program.cs
+++ EjerciciosBásicos/EjerciciosBásicos/Program.cs
@@ -1,12 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Security.Cryptography.X509Certificates;
-
-internal class Program
+﻿internal class Program
 {
     private static void Main(string[] args)
     {
         int n, a;
         int[] arr = { 2, 34, 66, 32, 99, 80, 23 };
+        string text = @"Historically, the world of data and the world of objects" +
+          @" have not been well integrated. Programmers work in C# or Visual Basic" +
+          @" and also in SQL or XQuery. On the one side are concepts such as classes," +
+          @" objects, fields, inheritance, and .NET APIs. On the other side" +
+          @" are tables, columns, rows, nodes, and separate languages for dealing with" +
+          @" them. Data types often require translation between the two worlds; there are" +
+          @" different standard functions. Because the object world has no notion of query, a" +
+          @" query can only be represented as a string without compile-time type checking or" +
+          @" IntelliSense support in the IDE. Transferring data from SQL tables or XML trees to" +
+          @" objects in memory is often tedious and error-prone.";
         /*
         //Detectar si un num es primo o no:
         Console.WriteLine("Introduce un número para comprovar si es primo : ");
@@ -21,7 +28,7 @@ internal class Program
         n = int.Parse(Console.ReadLine());
         a = int.Parse(Console.ReadLine());
         Console.WriteLine($"la multiplicación de {n} por {a} es igual a : {Multiplicar(n,a)}");
-        */
+      
         //Obtener el mayor num de un array recorriendola solo 1 vez
         Console.WriteLine("\n Dado el siguiente array : ");
         foreach(int aux in arr)
@@ -31,9 +38,39 @@ internal class Program
         
         Console.WriteLine("\nEl mayor número de la array es : " + Mayor2(arr));
         Console.WriteLine("\nCon linq; El mayor número de la array es : " + arr.Max());
+          */
+        //Contar las veces que se repite una palabras en una frase
 
+        ContarPalabrasRepetidas(text);
 
         Console.ReadKey();
+
+    }
+    public static int ContarPalabrasRepetidas(string txt)
+    {
+        //Paso la cadena 'text' a cadena de palabras:
+
+        string[] text = txt.Split(new char[] { '.', '?', '!', ' ', ';', ':', ',' }, StringSplitOptions.RemoveEmptyEntries);
+        List<string> Repeticiones = new List<string>();
+        Repeticiones.Add(text[0]);
+        Repeticiones.Add("1");
+        //Creo la query para buscar cada palabra en el texto
+        int y = 0;
+        for (int i = 0; i < text.Length; i++)
+        {
+            Repeticiones.Add(text[i]);
+            Repeticiones.Add("0");
+            text.Count();
+
+
+        }
+        
+
+        return text.Length;
+      
+
+        //bool Correcto = listaDeNumeros.Contains(100);    //Contains nos dice si un elemento existe o no en la lista
+        //static List<int> listaDeNumeros = new List<int> { 5, 10, 20 };
 
     }
 
