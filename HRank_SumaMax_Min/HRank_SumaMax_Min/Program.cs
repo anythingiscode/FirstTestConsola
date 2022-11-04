3efd9d6c6d1141c94becc6cc11998f722200836f
anythingiscode <rolkawa99@gmail.com>
2022-11-04 13:07:21 +0100

    Ph1_Suma max y min de una array de 5 elementos de los que cogemos solo 4

---
+++ HRank_SumaMax_Min/HRank_SumaMax_Min/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+        Console.ReadKey();
+
+    }
+}
+
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    {
+        int sumaMax = 0, sumaMin = 0;
+        arr.Sort();     //Ordenom de menor a mayor
+        
+        for(int i = 0; i < 4; i++)
+        {
+            sumaMin = sumaMin + arr[i];
+        }
+        arr.Reverse();      //Invierto la cade`na para ordenar de mayor a menor
+
+        for (int i = 0; i < 4; i++)
+        {
+            sumaMax = sumaMax + arr[i];
+        }
+
+        Console.Write(sumaMin + " " + sumaMax);
+
+    }
+
+}
