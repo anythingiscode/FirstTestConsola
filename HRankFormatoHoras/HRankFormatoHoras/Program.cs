e18fd6bf8d33b225e4ee1ec4b90be3ee585cb77b
anythingiscode <rolkawa99@gmail.com>
2022-11-07 16:17:27 +0100

    Cambiar formato 12h a 24H

---
+++ HRankFormatoHoras/HRankFormatoHoras/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+       
+
+        string result = Result.timeConversion(s);
+        Console.WriteLine(result);
+        Console.ReadKey();
+
+        //textWriter.WriteLine(result);
+
+        //textWriter.Flush();
+        //textWriter.Close();
+    }
+}
+class Result
+{
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string timeConversion(string s)
+    {
+        DateTime d = DateTime.Parse(s);
+        return d.ToString("HH:mm:ss");
+    }
+
+}
