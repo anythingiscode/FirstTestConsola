0f16de7073099009f11f3cfb8b190e9208862648
anythingiscode <rolkawa99@gmail.com>
2022-11-08 11:54:04 +0100

    Dado una serie de numeros enteros desde 1 hasta n --> indicar con fizz si es multiplo de 3, con buzz su es multiplo de 5 y sino imprimir el numero. Todo ello en una línea por num

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,42 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Result.fizzBuzz(100);
+        Console.ReadKey();
+    }
+}
+class Result
+{
+
+    /*
+     * Complete the 'fizzBuzz' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void fizzBuzz(int n)
+    {
+        bool fizz = false;
+        bool buzz = false;
+        for (int i = 1; i <= n; i++)
+        {
+            fizz = false;
+            buzz = false;
+
+            if (i % 3 == 0)
+            {
+                fizz = true;
+            }
+            if (i % 5 == 0)
+            {
+                buzz = true;
+            }
+
+            if (fizz && buzz) Console.WriteLine("FizzBuzz");
+            else if (fizz && !buzz) Console.WriteLine("Fizz");
+            else if (!fizz && buzz) Console.WriteLine("Buzz");
+            else Console.WriteLine(i);
+        }
+    }
+}
