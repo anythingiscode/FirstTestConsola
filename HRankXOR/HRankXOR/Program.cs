e2f104d76e3d4e666ca3c05fbcb1ada9befcc5cf
anythingiscode <rolkawa99@gmail.com>
2022-11-10 17:31:09 +0100

    sinOUt

---
+++ HRankXOR/HRankXOR/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //List<int> result = new List<int>();
+        string result = string.Empty;
+        string XOR = string.Empty;
+        List<int> s = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(sTemp => Convert.ToInt32(sTemp)).ToList();
+        List<int> t = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(sTemp => Convert.ToInt32(sTemp)).ToList();
+        for (int i = 0; i < s.Count; i++)
+        {
+            XOR = s[i] == t[i] ? "1" : "0";
+            result = result + XOR;
+        }
+        Console.WriteLine(result);
+        Console.ReadKey();
+    }
+}
