9a95df9b91701653d1671e36503bfddcf88a41e5
anythingiscode <rolkawa99@gmail.com>
2022-11-10 11:55:25 +0100

    sos

---
+++ HRankMsgSOS_CompTxt/HRankMsgSOS_CompTxt/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string s = "SOSSFSEOS";
+        string model = "SOS";
+        int contador = 0;
+        for (int i = 0; i < s.Length/3; i++)
+        {
+            for(int j = 0; j < 3; j++)
+            {
+                if (s[i*3+j] != model[j]) contador++; 
+            }
+        }
+
+        Console.WriteLine(contador);
+        Console.ReadKey();
+    }
+}
