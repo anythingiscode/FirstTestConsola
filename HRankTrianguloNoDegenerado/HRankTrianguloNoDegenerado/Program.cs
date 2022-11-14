d9c02294b023522657b5cf94d64b10444011d002
anythingiscode <rolkawa99@gmail.com>
2022-11-14 20:23:07 +0100

    uso clase y double en lugar de int ya que la suma de los lados puede desbordar la capacidad de int32

--- HRankTrianguloNoDegenerado/HRankTrianguloNoDegenerado/Program.cs
+++ HRankTrianguloNoDegenerado/HRankTrianguloNoDegenerado/Program.cs
@@ -16,6 +16,38 @@
         textWriter.Close();
     }
 }
+class Triangulo
+{
+    private double _a;
+    private double _b;
+    private double _c;   
+
+    public double A { get => _a; set => _a = value; }
+    public double B { get => _b; set => _b = value; }
+    public double C { get => _c; set => _c = value; }
+
+    public Triangulo(double a, double b, double c)
+    {
+        A = a;
+        B = b;
+        C = c;
+    }
+
+    public Triangulo()
+    {
+
+    }
+
+    public bool TDegenerado(double a, double b, double c)
+    {
+        bool TD = (a + b > c) && (b + c > a) && (c + a > b);
+        return !TD;
+    }
+    public double LongTr(double a, double b, double c)
+    {
+        return a + b + c;
+    }
+}
 class Result
 {
 
@@ -25,15 +57,91 @@ class Result
      * The function is expected to return an INTEGER_ARRAY.
      * The function accepts INTEGER_ARRAY sticks as parameter.
      */
-    public static bool TDegenerado(int a, int b, int c)
-    {
-        bool TD = (a + b > c) && (b + c > a) && (c + a > b);
-        return TD;
-    }
+   
 
     public static List<int> maximumPerimeterTriangle(List<int> sticks)
     {
+        List<Triangulo> ListaTND = new List<Triangulo>();
+        List<int> resultadoTr = new List<int>();
+        Triangulo Tr = new Triangulo();
+        double longitudMax = 0;
+
+        for(int i = 0; i < sticks.Count; i++)
+        {
+            for(int j = i + 1; j < sticks.Count; j++)
+            {
+                for(int k = j + 1; k < sticks.Count; k++)
+                {
+                    //Console.WriteLine($"--> {sticks[i]} {sticks[j]} {sticks[k]}");
+
+                    if (!Tr.TDegenerado(sticks[i], sticks[j], sticks[k]))
+                    {                    
+                        if(longitudMax <= Tr.LongTr(sticks[i], sticks[j], sticks[k]))
+                        {
+                            longitudMax = Tr.LongTr(sticks[i], sticks[j], sticks[k]);
+                            ListaTND.Add(new Triangulo(sticks[i], sticks[j], sticks[k]));
+                        }  
+                    }
+                }
+            }
+        }
+
+        //foreach(Triangulo tr in ListaTND)
+        //{
+        //    Console.WriteLine($"ListaTND antes de filtrar>> {tr.A} {tr.B} {tr.C}");
+        //}
+        //Console.ReadKey();
+                
+
+        if (longitudMax != 0)
+        {
+            if (ListaTND.Count() == 1)
+            {
+                resultadoTr.Add(Convert.ToInt32(ListaTND[0].A));
+                resultadoTr.Add(Convert.ToInt32(ListaTND[0].B));
+                resultadoTr.Add(Convert.ToInt32(ListaTND[0].C));
+                resultadoTr.Sort();
+            }
+            else
+            {
+                IEnumerable<Triangulo> TrMaxLong = from Triangulo t in ListaTND
+                                                   where t.A + t.B + t.C == longitudMax
+                                                   select t;                
+
+                Tr = TrMaxLong.ElementAt(0);
+                resultadoTr.Add(Convert.ToInt32(Tr.A));                
+                resultadoTr.Add(Convert.ToInt32(Tr.B));               
+                resultadoTr.Add(Convert.ToInt32(Tr.C));
+                resultadoTr.Sort();
+
+
+                //foreach (Triangulo tr in TrMaxLong)
+                //{
+                //    Console.WriteLine($"\nTrMaxLong : {tr.A} {tr.B} {tr.C}");
+                //}
+                //Console.ReadKey();      
+
+                foreach (int res in resultadoTr)
+                {
+                    Console.WriteLine($"\nResultado : {res} ");
+                }
+                Console.ReadKey();               
+            }        
+        }
+        else
+        {
+            resultadoTr.Add(-1);         
+        }
+
         
+
+        foreach (int tr in resultadoTr)
+        {
+            Console.Write($"Resultado ==> {tr} ");
+        }
+        Console.ReadKey();
+        
+        return resultadoTr;
 
     }
 
