da2b96943ed3976cd23af6f6255ec890d8a17674
anythingiscode <rolkawa99@gmail.com>
2022-11-16 09:14:54 +0100

    Canguros saltando hasta que se encuentran

---
+++ HRankSaltoDeCanguro/HRankSaltoDeCanguro/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter("FicheroEjuemplo");
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int x1 = Convert.ToInt32(firstMultipleInput[0]);
+
+        int v1 = Convert.ToInt32(firstMultipleInput[1]);
+
+        int x2 = Convert.ToInt32(firstMultipleInput[2]);
+
+        int v2 = Convert.ToInt32(firstMultipleInput[3]);
+
+        string result = Result.kangaroo(x1, v1, x2, v2);
+
+        textWriter.WriteLine(result);
+        Console.WriteLine(result);
+        Console.ReadKey();
+
+        textWriter.Flush();
+        textWriter.Close();
+
+    }
+}
+class Result
+{
+
+    /*
+     * Complete the 'kangaroo' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER x1
+     *  2. INTEGER v1
+     *  3. INTEGER x2
+     *  4. INTEGER v2
+     */
+
+    public static string kangaroo(int x1, int v1, int x2, int v2)
+    {
+        try
+        {
+            if ((x2 - x1) % (v1 - v2) == 0 && (x2 - x1) / (v1 - v2) > 0)
+            {
+                return "YES";
+            }
+            else
+                return "NO";            
+        }
+        catch (Exception)
+        {
+
+            return "NO";
+        }      
+    }
+
+}
