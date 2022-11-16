dccc492c792e8159e63a497b54384a61f72aaf9b
anythingiscode <rolkawa99@gmail.com>
2022-11-16 15:31:49 +0100

    TowerBreaks

---
+++ HRankTowerBreakers/HRankTowerBreakers/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter("FicheroEjuemplo");
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int n = Convert.ToInt32(firstMultipleInput[0]);
+
+            int m = Convert.ToInt32(firstMultipleInput[1]);
+
+            int result = Result.towerBreakers(n, m);
+
+            Console.WriteLine("\n\n Resultado : " + result);
+            Console.ReadKey();
+
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
+class Result
+{
+
+    /*
+     * Complete the 'towerBreakers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER m
+     */
+
+    public static int towerBreakers(int n, int m)
+    {
+        if (n % 2 == 0)
+        {
+            return 2;
+        }
+        else
+        {
+            if (m == 1)
+            {
+                return 2;
+            }
+            return 1;
+        }
+    }
+
+}
