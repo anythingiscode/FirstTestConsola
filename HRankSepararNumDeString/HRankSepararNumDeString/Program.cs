b0555997ef12dbac46b68f97c4fd4eeed028df04
anythingiscode <rolkawa99@gmail.com>
2022-11-16 12:16:42 +0100

    No es mi codigo y no creo realmente que sea correcto : s="99100" da solucion "NO" y yo creo que es SI 99 =>"99+100"

---
+++ HRankSepararNumDeString/HRankSepararNumDeString/Program.cs
@@ -0,0 +1,52 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string s = Console.ReadLine();
+            Console.WriteLine("\nsolucion " + qItr);
+            Result.separateNumbers(s);
+            
+            Console.ReadKey();
+        }
+    }
+}
+class Result
+{
+
+    /*
+     * Complete the 'separateNumbers' function below.
+     *
+     * The function accepts STRING s as parameter.
+     */
+
+    public static void separateNumbers(string s)
+    {
+
+        long b = 0;
+        string a = "", c = "";
+
+        for (int i = 1; i <= s.Length / 2; i++)
+        {
+            a = "";
+            c = s.Substring(0, i);
+            b = long.Parse(c);
+
+            while (a.ToString().Length < s.Length)
+            {
+                a += b;
+                b++;
+            }
+            if (a.Equals(s))
+            {
+                Console.WriteLine("YES " + c);
+                return;
+            }
+        }
+        Console.WriteLine("NO ");
+        
+    }
+}
