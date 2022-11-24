9d7d03ac97752152c4eb13aa75e9d5ba3774cc71
anythingiscode <rolkawa99@gmail.com>
2022-11-24 19:28:21 +0100

    Solucion que funciona pero que excede el tiempo limite.-->optimizar

---
+++ HRankJuegoQuitarMayorNumArray/HRankJuegoQuitarMayorNumArray/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter("FicheroEjercicio.txt");
+
+        int g = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int gItr = 0; gItr < g; gItr++)
+        {
+            int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            string result = Result.gamingArray(arr);
+
+            textWriter.WriteLine(result);
+
+            Console.WriteLine(result + "\n");
+        }
+        Console.ReadKey();
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
+/*
+ Juego en le que hay que quitar de una array el num mayor y todos los elementos a su drcha.
+El último que tira gana. Ej: [1,4,10,3,5] --> [1,4] --> [1]
+*/
+class Result
+{
+
+    /*
+     * Complete the 'gamingArray' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static string gamingArray(List<int> arr)
+    {
+        int nx = arr.Capacity;
+        int cont = 0;
+        while (nx > 0)
+        {
+            arr = arr.GetRange(0, nx - (nx - arr.IndexOf(arr.Max())));
+            nx = arr.Count;
+            cont++;
+        }
+            
+        return (cont % 2 == 0) ? "ANDY" : "BOB";
+    }
+
+}
