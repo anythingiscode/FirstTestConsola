36be9b197710d4d8aae22931ee67d2ebd8b7f2ac
anythingiscode <rolkawa99@gmail.com>
2022-11-24 11:45:50 +0100

    Primer intento

---
+++ HRankSansaXOR/HRankSansaXOR/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.Numerics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter("FicheroEjercicio.txt");
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {       
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            int result = Result.sansaXor(arr);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
+class Result
+{
+
+    /*
+     * Complete the 'sansaXor' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int sansaXor(List<int> arr)
+    {
+        int result = 0;
+        result = arr[0] ^ arr[1];
+        string cadena = "Ejemplo";
+        List<char> lstChars = cadena.ToList();
+        Console.WriteLine("Chars[0] : " + Convert.ToString(lstChars));
+
+        Console.WriteLine("Resultado : " + Convert.ToString(result));
+        Console.ReadKey();
+        return 0;
+    }
+
+}
---
+++ HRankSumIzqDrcha=/HRankSumIzqDrcha=/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter("FicheroEjercicio.txt");
+        int T = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int TItr = 0; TItr < T; TItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            string result = Result.balancedSums(arr);
+
+            textWriter.WriteLine(result);
+            Console.WriteLine(result);
+        }
+        Console.ReadKey();
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
+class Result
+{
+
+    /*
+     * Complete the 'balancedSums' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static string balancedSums(List<int> arr)
+    {
+        int nx = arr.Count;
+        int sumIzq = 0, sumDcha = 0;
+        List<int> arrIzq = new List<int>();
+        List<int> arrDcha = new List<int>();
+
+        for (int i = 2; i < nx - 1; i++)
+        {
+            arrIzq = arr.GetRange(0,i - 1);
+            arrDcha = arr.GetRange(i + 1, nx);
+            sumIzq = arrIzq.Sum();
+            sumDcha = arrDcha.Sum();
+            if (sumIzq == sumDcha) return "YES";
+        }
+        return "NO";
+    }
+
+}
