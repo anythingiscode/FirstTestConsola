a6a98737f44c82c04109026e427c9fb0b9a196ce
anythingiscode <rolkawa99@gmail.com>
2022-11-24 14:40:40 +0100

    OPcion comptacta

--- HRankSumIzqDrcha=/HRankSumIzqDrcha=/Program.cs
+++ HRankSumIzqDrcha=/HRankSumIzqDrcha=/Program.cs
@@ -35,6 +35,7 @@ class Result
 
     public static string balancedSums(List<int> arr)
     {
+        /*
         if (arr.Count == 1)
             return "YES";
         int sumTotal = arr.Sum();
@@ -46,28 +47,38 @@ class Result
             int remained = sumTotal - arr[i];
 
             if (i == 1 && (sumTotal - arr[i - 1]) == 0)
+            {
+                Console.WriteLine(Convert.ToString(arr[i]));
+                Console.WriteLine(Convert.ToString(i));
                 return "YES";
+            }
+                
 
             if (remained % 2 == 0 && remained / 2 == sum)
+            {
+                Console.WriteLine(">> " + Convert.ToString(arr[i]));
+                Console.WriteLine(">> " + Convert.ToString(i));
                 return "YES";
-        }
-        return "NO";
-        /*
-        int nx = arr.Capacity;
-        int sumIzq = 0, sumDcha = 0;
-        List<int> arrIzq = new List<int>();
-        List<int> arrDcha = new List<int>();        
-
-        for (int i = 1; i < nx - 1; i++)
-        {
-            arrIzq = arr.GetRange(0,i);
-            arrDcha = arr.GetRange(i + 1, nx - i - 1);
-            sumIzq = arrIzq.Sum();
-            sumDcha = arrDcha.Sum();
-            if (sumIzq == sumDcha) return "YES";
+            }
+                
         }
         return "NO";
         */
+       
+        int nx = arr.Capacity;
+        int sumIzq = 0, sumDcha = 0;     
+      
+        if (nx == 1)
+            return "YES";
+
+        for (int i = 1; i < nx - 1; i++)
+        {          
+            sumIzq = arr.GetRange(0, i).Sum();
+            sumDcha = arr.GetRange(i + 1, nx - i - 1).Sum();
+            if (sumIzq == sumDcha) return "YES";
+        }
+        return "NO";
+       
     }
 
 }
