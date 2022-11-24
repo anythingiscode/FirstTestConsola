9b9c516b7f91a3c6e86520ed27e76e47f739af39
anythingiscode <rolkawa99@gmail.com>
2022-11-24 17:22:11 +0100

    You can NOT use Arrays and .reduce() to compare sums, cause 2 tests will fail = too slow FORMULA FOR SUCCESS: create a TOTAL = which the sum of ALL elements in the array create SUM, which adds Array elements 1 by 1 as we for...loop them if at any point: SUM === (Total Array Sum) - SUM - arr[i] ... both sides are balanced, so exit out of the function with return 'YES' else return 'NO'

--- HRankSumIzqDrcha=/HRankSumIzqDrcha=/Program.cs
+++ HRankSumIzqDrcha=/HRankSumIzqDrcha=/Program.cs
@@ -35,47 +35,16 @@ class Result
 
     public static string balancedSums(List<int> arr)
     {
-        /*
-        if (arr.Count == 1)
-            return "YES";
-        int sumTotal = arr.Sum();
-        int sum = 0;
-
-        for (int i = 1; i < arr.Count; i++)
-        {
-            sum += arr[i - 1];
-            int remained = sumTotal - arr[i];
-
-            if (i == 1 && (sumTotal - arr[i - 1]) == 0)
-            {
-                Console.WriteLine(Convert.ToString(arr[i]));
-                Console.WriteLine(Convert.ToString(i));
-                return "YES";
-            }
-                
-
-            if (remained % 2 == 0 && remained / 2 == sum)
-            {
-                Console.WriteLine(">> " + Convert.ToString(arr[i]));
-                Console.WriteLine(">> " + Convert.ToString(i));
-                return "YES";
-            }
-                
-        }
-        return "NO";
-        */
-       
+        
         int nx = arr.Capacity;
-        int sumIzq = 0, sumDcha = 0;     
-      
-        if (nx == 1)
-            return "YES";
+        int sumTotal = arr.Sum();
+        int sum = 0, resto = 0;       
 
-        for (int i = 1; i < nx - 1; i++)
-        {          
-            sumIzq = arr.GetRange(0, i).Sum();
-            sumDcha = arr.GetRange(i + 1, nx - i - 1).Sum();
-            if (sumIzq == sumDcha) return "YES";
+        for (int i = 0; i < nx; i++)
+        {       
+            resto = sumTotal - sum - arr[i];           
+            if (sum == resto) return "YES";
+            sum += arr[i];
         }
         return "NO";
        
